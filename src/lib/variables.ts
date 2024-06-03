689b23d31a86f1adc7d1b3f77bceac44f3e8a457
thunder_fury <whw3312@gmail.com>
2024-06-04 00:37:44 +0900

    env key変更

--- src/lib/variables.ts
+++ src/lib/variables.ts
@@ -1,9 +1,9 @@
-const notionUsersBaseToken = process.env.NOTION_USERSBASE_TOKEN
-const usersDatabaseId = process.env.NOTION_USERS_DATABASE_ID
+const notionUsersBaseToken = process.env.NEXT_PUBLIC_NOTION_USERSBASE_TOKEN
+const usersDatabaseId = process.env.NEXT_PUBLIC_NOTION_USERS_DATABASE_ID
 //
-const medicalViitHistoryToken = process.env.NOTION_MEDICAL_VISIT_HISTORY_TOKEN
-const medicalViitHistoryId = process.env.NOTION_MEDICAL_VISIT_HISTORY_ID
-const accessToken = process.env.ACCESS_TOKEN_SECRET
+const medicalViitHistoryToken = process.env.NEXT_PUBLIC_NOTION_MEDICAL_VISIT_HISTORY_TOKEN
+const medicalViitHistoryId = process.env.NEXT_PUBLIC_NOTION_MEDICAL_VISIT_HISTORY_ID
+const accessToken = process.env.NEXT_PUBLIC_ACCESS_TOKEN_SECRET
 const headers = (notionToken?: string) => {
   return {
     Accept: 'application/json',
