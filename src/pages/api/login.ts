f9ebd0ae837ae24e3f1457428c0c1b7e4fbd6d15
thunder_fury <whw3312@gmail.com>
2024-06-04 11:57:24 +0900

    ログインタイプ定義

--- src/pages/api/login.ts
+++ src/pages/api/login.ts
@@ -5,7 +5,10 @@ import bcrypt from 'bcrypt'
 import jwt from 'jsonwebtoken'
 import { notionDataFormatting } from "@/lib/notionDataFormatting";
 import { revalidatePath } from "next/cache"
-
+type RichTextType = { rich_text: { plain_text: string }[] }
+type PropertiesType = {
+  properties: Record<'mail' | 'password', RichTextType>;
+}
 type Users = {
   mail: string
   password: string
@@ -30,20 +33,22 @@ export default async function handler(
   )
   try {
     const data = await respons.json()
-    const users = data.results.map((item: any) => ({
-      mail: item.properties.mail.rich_text.map((text: any) => text.plain_text).join(),
-      password: item.properties.password.rich_text.map((text: any) => text.plain_text).join()
-    }))
+    const usersComfim = data.results.map((item: PropertiesType) => {
+      console.log(item.properties.mail.rich_text)
+      return ({
+      mail: item.properties.mail.rich_text.map((text) => text.plain_text).join(),
+      password: item.properties.password.rich_text.map((text) => text.plain_text).join()
+    })})
     console.log(req.body)
-    const user = users.find((user: Users) => user.mail === mail)
-    if (!user) {
+    const userCheck = usersComfim.find((user: Users) => user.mail === mail)
+    if (!userCheck) {
       if (mail === undefined) {
         res.status(401).json({ message: '메일을 입력해주세요', status: 401 })
       }
       res.status(402).json({ message: '등록된 메일이 없습니다', status: 402 })
     }
     if (!accessToken) return
-    const isMatch = await bcrypt.compare(password, user.password as string)
+    const isMatch = await bcrypt.compare(password, userCheck.password as string)
     if (isMatch) {
       const token = jwt.sign({ id: mail }, accessToken, {
         expiresIn: `24h`,
